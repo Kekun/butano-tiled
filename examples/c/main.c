eaf8ed8bef0e66528dcdad98e78475ee386bf1c2
Adrien Plazas <kekun.plazas@laposte.net>
2023-10-02 20:55:12 +0200

    Add a C example
    Signed-off-by: Adrien Plazas <kekun.plazas@laposte.net>

---
+++ examples/c/main.c
@@ -0,0 +1,12 @@
+/*
+ * Copyright (c) 2023 Adrien Plazas <kekun.plazas@laposte.net>
+ * zlib License, see LICENSE file.
+ */
+
+#include <bntmx.h>
+#include <bntmx_maps_wonderland.h>
+
+int main()
+{
+    return 0;
+}
